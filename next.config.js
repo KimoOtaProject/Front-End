283244a8bc71d0562ef092928cad6d15d8e81938
CyanLH <dlgksk5@gmail.com>
2021-05-30 18:36:33 +0900

    fix redux, webpack error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const { i18n } = require('./next-i18next.config')
-const withLess = require('@zeit/next-less');
 
-// fix: prevents error when .less files are required by node
-if (typeof require !== 'undefined') {
-    require.extensions['.less'] = file => {};
-}
-
-module.exports = withLess({
+module.exports = {
     i18n,
-    lessLoaderOptions: {
-        javascriptEnabled: true,
-    },
-})
+}
--- reducers/test.ts
+++ reducers/test.ts
@@ -9,10 +9,12 @@ const testSlice = createSlice({
     name: 'test', // slice 이름
     initialState, // slice 최초 state
     reducers: { // test 의 동작(reducer)
-        changeValue: (state, action) => state.value = action.payload, // value 값을 변경하는 동작
+        changeValue: (state, action) => {
+            state.value = action.payload;
+        }, // value 값을 변경하는 동작
     },
     // extraReducers 는 비동기 동작을 수행하는 reducer 임.
-    extraReducers: builder =>  {},
+    // extraReducers: builder =>  {},
 });
 
 // 이겨서 export 한 reducer 들을 component 에서 가져와 dispatch 합수로 감싸 사용하면 변경이 저장됨.
