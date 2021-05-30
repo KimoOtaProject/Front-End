b5e8ae44c2531cac6da1f5de72175b65500e281a
CyanLH <dlgksk5@gmail.com>
2021-05-30 18:13:22 +0900

    add redux change example

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const { i18n } = require('./next-i18next.config')
+const withLess = require('@zeit/next-less');
 
-module.exports = {
-    i18n,
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+    require.extensions['.less'] = file => {};
 }
+
+module.exports = withLess({
+    i18n,
+    lessLoaderOptions: {
+        javascriptEnabled: true,
+    },
+})
--- reducers/index.ts
+++ reducers/index.ts
@@ -2,6 +2,13 @@ import {combineReducers} from "@reduxjs/toolkit";
 
 import test from './test';
 
+// 우리의 redux 에서 사용될 state interface
+export interface rootState {
+    test: {
+        value: number,
+    },
+};
+
 // 작성한 reducer 들을 하나로 합쳐 export
 export default combineReducers({
     test: test.reducer,
